29bdab4dc65141dab9be943d4b0a8e516cfbedf9
Nichole <nicholemcgrew@gmail.com>
2023-04-10 08:03:44 -0500

    Simple Change Machine

--- codewars-main/Codewars/main.js
+++ codewars-main/Codewars/main.js
@@ -3021,4 +3021,16 @@ function uefaEuro2016(teams, scores){
     return `At match ${teams[0]} - ${teams[1]}, teams played draw.`
   }
 }
-console.log(uefaEuro2016(['Belgium', 'Italy'], [0, 2]))
+// console.log(uefaEuro2016(['Belgium', 'Italy'], [0, 2]))
+
+//Simple Change Machine
+function changeMe(moneyIn){
+  switch(moneyIn) {
+    case '£5': return '20p '.repeat(25).trim()
+    case '£2': return '20p '.repeat(10).trim()
+    case '£1': return '20p '.repeat(5).trim()
+    case '50p': return '20p 20p 10p'
+    case '20p': return '10p 10p'
+    default: return moneyIn
+  }
+}
