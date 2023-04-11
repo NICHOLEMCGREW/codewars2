b36d0e9a8c94873ff0f6aac9c6fff67ece39b199
Nichole <nicholemcgrew@gmail.com>
2023-04-11 08:34:45 -0500

    For UFC Fans (Total Beginners): Conor McGregor vs George Saint Pierre

--- codewars-main/Codewars/main.js
+++ codewars-main/Codewars/main.js
@@ -3034,3 +3034,9 @@ function changeMe(moneyIn){
     default: return moneyIn
   }
 }
+
+// For UFC Fans (Total Beginners): Conor McGregor vs George Saint Pierre
+function quote (fighter) {
+  return fighter.toLowerCase() === 'george saint pierre' ? "I am not impressed by your performance." : "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+};
+console.log(quote('George Saint Pierre'))
