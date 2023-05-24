ed70b7cb108ee0fb166877a15fba7c0eece43aec
Nichole <nicholemcgrew@gmail.com>
2023-05-24 08:37:52 -0500

    Area of a Square

--- codewars-main/Codewars/main.js
+++ codewars-main/Codewars/main.js
@@ -2753,3 +2753,12 @@ function sumMul(n,m){
   // Surface Area and Volume of a Box
 
   getSize = (width, height, depth) => [2 * (width * height + width * depth + height * depth), width * height * depth]
+
+  // Area of a Square
+  function squareArea(A){
+    return +Math.pow(2 * A / Math.PI, 2).toFixed(2)
+  }
+
+  circumfrence = 4*A = 2*pi*R
+  Radius = 4*A/(2*pi) = 2*A/pi
+  area = 
