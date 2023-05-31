e907924dda92defeee1b3cbbad2f744a53c8540c
Nichole <nicholemcgrew@gmail.com>
2023-05-31 10:08:26 -0500

    Do you speak English?

--- codewars-main/Codewars/main.js
+++ codewars-main/Codewars/main.js
@@ -2670,3 +2670,9 @@ function check(a,x){
     }
 };
 console.log(check([80,117,115,104,45,85,112,115], 45))
+
+// Do you speak "English"?
+function spEng(sentence){
+  sentence = sentence.toLowerCase();
+  return sentence.includes('english');
+  }
