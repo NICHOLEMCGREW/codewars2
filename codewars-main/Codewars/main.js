68475c8cfb403a4b8ae08fda7bf12496ca3a4e94
Nichole <nicholemcgrew@gmail.com>
2023-06-02 08:37:07 -0500

    L1: Bartender, drinks!

--- codewars-main/Codewars/main.js
+++ codewars-main/Codewars/main.js
@@ -2642,3 +2642,18 @@ function remainder(n, m){
 var Ball = function(ballType) {
   this.ballType = ballType || 'regular'
 };
+
+// L1: Bartender, drinks!
+function getDrinkByProfession(param){
+  param = param.toLowerCase();
+  
+  switch(param) {
+    case "jabroni": return "Patron Tequila";
+    case "school counselor": return "Anything with Alcohol";
+    case "programmer": return "Hipster Craft Beer";
+    case "bike gang member": return "Moonshine";
+    case "politician": return  "Your tax dollars";
+    case "rapper": return "Cristal";
+    default: return "Beer";
+  }
+}
