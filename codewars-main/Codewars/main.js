e3f729e056d546e94fb53c328f8f4003584dacb3
Nichole <nicholemcgrew@gmail.com>
2023-06-13 07:55:13 -0500

    Determine offspring sex based on genes XX and XY chromosomes

--- codewars-main/Codewars/main.js
+++ codewars-main/Codewars/main.js
@@ -2546,3 +2546,13 @@ function  calculateAge(birthYear, currentYear) {
   return `You will be born in ${diff * -1} years.`
   }
   
+  
+  // Determine offspring sex based on genes XX and XY chromosomes
+
+  function chromosomeCheck(sperm) {
+    if (sperm.includes('Y')) {
+      return "Congratulations! You're going to have a son."
+    } else {
+      return "Congratulations! You're going to have a daughter."
+    }
+  }
