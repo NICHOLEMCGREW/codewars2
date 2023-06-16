4dcae3ace665c783c1352c24a35189e742de4210
Nichole <nicholemcgrew@gmail.com>
2023-06-16 09:18:27 -0500

    How many lightsabers do you own?

--- codewars-main/Codewars/main.js
+++ codewars-main/Codewars/main.js
@@ -2518,3 +2518,13 @@ function include(arr, item){
     return false;
   }
 }
+
+// How many lightsabers do you own?
+
+function howManyLightsabersDoYouOwn(name) {
+  if (name == 'Zach') {
+    return 18;
+  } else {
+    return 0;
+  }
+}
