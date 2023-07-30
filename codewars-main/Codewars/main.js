901cbf3f58f822db9e62b89b442ac3a9c7b1dca3
Nichole <nicholemcgrew@gmail.com>
2023-07-30 08:13:46 -0500

    sort and star

--- codewars-main/Codewars/main.js
+++ codewars-main/Codewars/main.js
@@ -2180,4 +2180,3 @@ function getAge(inputString){
 function twoSort(s) {
   return s.sort()[0].split('').join('***')
 }
-console.log(twoSort(["bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps"]))
