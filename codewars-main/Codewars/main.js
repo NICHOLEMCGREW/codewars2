da6d53fc0c000258ac0442969cdc4c7c2f454ede
Nichole <nicholemcgrew@gmail.com>
2023-08-06 11:53:12 -0500

    codewars problems

--- codewars-main/Codewars/main.js
+++ codewars-main/Codewars/main.js
@@ -984,7 +984,61 @@ function findMultiples(integer, limit) {
     return multiples;
   }
   
-console.log(findMultiples(5, 25))
+// console.log(findMultiples(5, 25))
 
 
-  
+  //Arrays
+//Your pokemon party order which is a list of pokemon has been leaked to Misty. Please create a function that reverses your list and prints it to the console. 
+
+// let list = ['yes', 'no', 'maybe']
+
+function reverseList(list) {
+    // let reversed = list.split(' ').reverse().join(' ')
+    return list.reverse();
+}
+
+// console.log(reverseList(['yes', 'no', 'maybe']))
+
+//Given two integer arrays a, b, both of length >= 1, create a program that returns true if the sum of the squares of each element in a is strictly greater than the sum of the cubes of each element in b.
+
+// let a = [1, 2, 4, 5]
+// let b = [2, 3, 4, 5]
+
+function greaterThan(a, b) {
+   
+   const newA = a.reduce((acc, curr) => acc + curr ** 2, 0)
+   const newB = b.reduce((acc, curr) => acc + curr ** 3, 0)
+
+   if (newA > newB) {
+    console.log('true')
+   } else {
+    console.log('b is bigger!')
+   }    
+}
+// greaterThan([1, 2, 4, 5], [2, 3, 4, 5])
+
+
+//Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
+// Some cases:
+// [22, -6, 32, 82, 9, 25] =>  [-6, 32, 25]
+// [68, -1, 1, -7, 10, 10] => [-1, 10]
+
+let arr = [22, -6, 32, 82, 9, 25]
+
+function multiples() {
+    return arr.filter((element, index) => element == 0 || element % index === 0)
+}
+
+// console.log(multiples())
+
+
+
+
+//Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.Return your answer as a number.
+
+let array = ['2', 8, 9, '3', 20]
+
+function addNums() {
+       return array.reduce((acc, curr) => acc + Number(curr), 0)
+}
+// console.log(addNums())
