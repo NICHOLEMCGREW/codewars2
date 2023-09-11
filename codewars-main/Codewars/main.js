de47e11204112d2cc1cfc40003961b175e4b37bb
Nichole <nicholemcgrew@gmail.com>
2023-09-11 07:41:58 -0500

    Grasshopper - Grade book

--- codewars-main/Codewars/main.js
+++ codewars-main/Codewars/main.js
@@ -1924,4 +1924,21 @@ function sumArray(array) {
   }
 }
 
-console.log(sumArray([ 6, 2, 1, 8, 10 ]))
+// console.log(sumArray([ 6, 2, 1, 8, 10 ]))
+
+// Grasshopper - Grade book
+
+function getGrade (s1, s2, s3) {
+  let avg = (s1 + s2 + s3) / 3
+  if (avg >= 90) {
+    return 'A'
+  } else if (avg >= 80) {
+    return 'B'
+  } else if (avg >= 70) {
+    return 'C'
+  } else if (avg >= 60) {
+    return 'D'
+  } else if (avg < 60) {
+    return 'F'
+  }
+}
