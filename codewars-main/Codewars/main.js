8b206a4217cc0dd3253ed6949d65f204a0a66c6a
Nichole <nicholemcgrew@gmail.com>
2023-09-11 07:37:40 -0500

    Sum without highest and lowest number

--- codewars-main/Codewars/main.js
+++ codewars-main/Codewars/main.js
@@ -1905,4 +1905,23 @@ function getAverage(marks){
   return Math.round(Math.floor(marks))
 }
 
-console.log(getAverage([1,1,1,1,1,1,1,2]))
+// console.log(getAverage([1,1,1,1,1,1,1,2]))
+
+// Sum without highest and lowest number
+
+function sumArray(array) {
+  if (array == null) {
+    return 0;
+  } else if (array.length < 2) {
+    return 0;
+  } else {
+    array = array.sort(function(a,b) {return a - b});
+    let sum = 0;
+    for (let i = 1; i < array.length -1; i++) {
+      sum += array[i]
+    }
+    return sum;
+  }
+}
+
+console.log(sumArray([ 6, 2, 1, 8, 10 ]))
