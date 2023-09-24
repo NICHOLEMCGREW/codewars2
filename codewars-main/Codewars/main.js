dd36b392caf39740950303dec78ff266d27164bc
Nichole <nicholemcgrew@gmail.com>
2023-09-24 08:11:18 -0500

    Exclamation marks series #11: Replace all vowel to exclamation mark in the sentence

--- codewars-main/Codewars/main.js
+++ codewars-main/Codewars/main.js
@@ -2413,4 +2413,12 @@ function plural(n) {
  return n !== 1;
 }
 
-console.log(plural(0))
+// console.log(plural(0))
+
+// Exclamation marks series #11: Replace all vowel to exclamation mark in the sentence
+
+function replace(s){
+  return s.replace(/[aeiou]/gi, '!')
+}
+
+console.log(replace("Hi!"))
