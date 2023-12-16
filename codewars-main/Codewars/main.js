6aaae0ad986dda74f86e31c1f89d9a6911f2d7b1
Nichole <nicholemcgrew@gmail.com>
2023-12-16 16:45:36 -0600

    Moving Zeros To The End

--- codewars-main/Codewars/main.js
+++ codewars-main/Codewars/main.js
@@ -3527,6 +3527,18 @@ function pigIt(str){
 // pigIt('Pig latin is cool')
 
 // Moving Zeros To The End
+
+//Parameters: A single array with multiple types of elements including numbers, strings, booleans
+//Return: A single array with the same elements BUT the zeros are moved to the end of the array. The other elements should remain in the same location in the array. 
+//Examples: moveZeros([1,2,0,1,0,1,0,3,0,1]), [1, 2, 1, 1, 3, 1, 0, 0, 0, 0])
+//Pseudocode: iterate over the starting array and identify the zeros. We could take the exisiting zeros and move them to the end of the array or we could count the zeros, remove them, then append the correct number of zeros to the end of the array.
+//proposed solution - iterate through the original array and assign each value to a new array bucket depending on if zero or not. join the buckets together and output. 
+
 function moveZeros(arr) {
-  return
+  let catchall = []
+  let zeros = []
+  for (let i = 0; i < arr.length; i++) {
+      arr[i] === 0 ? zeros.push(arr[i]) : catchall.push(arr[i])
+  }
+  return ([...catchall, ...zeros])
 }
