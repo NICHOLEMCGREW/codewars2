784f01deee2c7dc8b8c189a13187e92a8e1c236e
Nichole McGrew <nicholemcgrew@gmail.com>
2024-02-09 09:40:27 -0600

    createCounter

--- LC.js
+++ LC.js
@@ -19,17 +19,3 @@ var createCounter = function(n) {
     };
 }; 
 
-class Counter { 
-    constructor(n) {
-        this.n = n;
-    }
-
-    increment() {
-        return ++this.n;
-    }
-}
-
-const counter = Counter(10)
-counter.increment()
-counter.increment()
-counter.increment()
