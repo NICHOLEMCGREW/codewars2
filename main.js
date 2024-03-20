7cbf423294e8b44c8e4163dd9899d07e7fa7590e
Nichole McGrew <nicholemcgrew@gmail.com>
2024-03-20 05:08:33 -0500

    reverse array

--- main.js
+++ main.js
@@ -4291,3 +4291,28 @@ const sumOfArray = sumContiguousArray2(n function(arr){
 	}
 	const nums = [1,2,3,4,5]
 	const sumOfArray2 = sumContiguousArray2(nums)
+
+
+	/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+    k = k % nums.length;
+
+	reverse(nums, 0, nums.length - 1)
+	reverse(nums, k, nums.length - 1)
+	reverse(nums, 0, k - 1)
+};
+
+let reverse = function(nums, start, end) {
+	while  (start < end) {
+		let temp = nums[start]
+		nums[start] = nums[end]
+		nums[end] = temp
+		start++
+		end--
+	}
+}
+	
