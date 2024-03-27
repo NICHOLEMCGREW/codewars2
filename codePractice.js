4e32cd641fdc5f05d19536ca1334c69341e87471
Nichole McGrew <nicholemcgrew@gmail.com>
2024-03-27 07:14:28 -0500

    matrix rotate image

--- codePractice.js
+++ codePractice.js
@@ -1,36 +1,18 @@
-/**
- * @param {number[]} nums
- * @param {number} k
- * @return {void} Do not return anything, modify nums in-place instead.
- */
-
-const reverseNums = (nums, start, end) => {
-    while(start < end) {
-        [nums[start], nums[end]] = [nums[end], nums[start]]
-        start++
-        end--
-    }
-}
-var rotate = function(nums, k) {
-    k = k % nums.length;
-
-    nums.reverse()
-    reverseNums(nums, 0, k - 1)
-    reverseNums(nums, k, nums.length - 1)
-};
-
-/**
- * @param {number[][]} matrix
- * @return {void} Do not return anything, modify matrix in-place instead.
- */
-var rotate = function(matrix) {
+//matrix rotate image
+// Create a function that rotates a 2D matrix 90 degrees clockwise 
+const rotate = function(matrix) {
+    //transpose the matrix
+    //iterate over each row
     for(let r = 0; r < matrix.length; r++) {
+        //iterate over each column
         for(let c = r; c < matrix[0].length; c++) {
+            //swap elements
             [matrix[r][c], matrix[c][r]] = [matrix[c][r], matrix[r][c]]
         }
     }
-
+    //iterate over each row of transposed matrix
     for (let row of matrix) {
+        //reverse rows and flip matrix
         row.reverse()
     }
-};
+}
