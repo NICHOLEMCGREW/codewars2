1615e842fb1a2b38641796fe8f0b634c3bb7a283
Nichole McGrew <nicholemcgrew@gmail.com>
2024-04-01 06:43:26 -0500

    Sum of positive

--- codePractice.js
+++ codePractice.js
@@ -134,3 +134,16 @@ function removeChar(str){
       function booleanToString(b) {
       return b ? 'true' : 'false'
       }      
+
+    //   Sum of positive
+    function positiveSum(arr) {
+        let sum = 0
+        for (let i = 0; i < arr.length; i++) {
+          if (arr[i] > 0) {
+            sum += arr[i]
+          }  
+        }
+        return sum
+    }
+
+    console.log(positiveSum([-1,2,3,4,5]))
