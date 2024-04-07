b18aee3d327e6dc2317da8dc8b10ec0c704f866e
Nichole McGrew <nicholemcgrew@gmail.com>
2024-04-07 07:40:00 -0500

    math

--- codePractice.js
+++ codePractice.js
@@ -211,3 +211,18 @@ function basicOp(operation, value1, value2){
     return value1 / value2
    }
 }
+
+function basicOp2(operation, value1, value2){
+  switch (operation) {
+    case '+':
+      return value1 + value2
+    case '-':
+      return value1 - value2
+    case '*':
+      return value1 * value2
+    case '/':
+      return value1 / value2
+      default:
+        return 0
+  }
+}
