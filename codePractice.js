8c1c290febe5e325f87d1d38e52b845e8c7efe7d
Nichole McGrew <nicholemcgrew@gmail.com>
2024-04-07 07:37:54 -0500

    Basic Mathematical Operations

--- codePractice.js
+++ codePractice.js
@@ -198,3 +198,16 @@ function doubleInteger(i) {
 function greet(name){
   return `Hello, ${name} how are you doing today?`
 }
+
+// Basic Mathematical Operations
+function basicOp(operation, value1, value2){
+  if (operation === '+') {
+   return value1 + value2
+  } else if (operation === '-') {
+    return value1 - value2
+   } else if (operation === '*') {
+    return value1 * value2
+   } else if (operation === '/') {
+    return value1 / value2
+   }
+}
