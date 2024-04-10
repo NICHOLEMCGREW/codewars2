b41b1f780b7699c92b0fb83b32163d8d909a0306
Nichole McGrew <nicholemcgrew@gmail.com>
2024-04-10 17:32:31 -0500

    counting sheep

--- codePractice.js
+++ codePractice.js
@@ -247,3 +247,14 @@ function paperwork(n, m) {
     return 0
   }
 }
+
+// Counting sheep...
+function countSheeps(sheep) {
+  let count = 0;
+  for (let i = 0; i < sheep.length; i++) {
+    if (sheep[i] === true) {
+      count += 1
+    }
+  }
+  return count
+}
