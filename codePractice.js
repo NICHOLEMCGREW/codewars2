e24eb85b84f16ebb3c48acf8fe2e8615e8788345
Nichole McGrew <nicholemcgrew@gmail.com>
2024-04-11 09:40:07 -0500

    Opposites Attract

--- codePractice.js
+++ codePractice.js
@@ -258,3 +258,21 @@ function countSheeps(sheep) {
   }
   return count
 }
+
+// Opposites Attract
+
+function lovefunc(flower1, flower2){
+  if (flower1 % 2 === 0 && flower2 % 2 !== 0 || flower1 % 2 !== 0 && flower2 % 2 === 0) {
+    return true
+  } else {
+    return false
+  }
+}
+
+// console.log(lovefunc(1,4))
+
+function lovefunc(flower1, flower2){
+  return flower1 % 2 !== flower2 % 2;
+}
+
+// Convert number to reversed array of digits
