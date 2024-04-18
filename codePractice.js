b8a79a9b4f143c12859846b929d9b92e8236f217
Nichole McGrew <nicholemcgrew@gmail.com>
2024-04-18 07:52:54 -0500

    refactor

--- codePractice.js
+++ codePractice.js
@@ -315,4 +315,8 @@ function findNeedle(haystack) {
     }
   }
 }
-console.log(findNeedle(['3', '123124234', undefined, 'needle', 'world', 'hay', 2, '3', true, false]))
+// console.log(findNeedle(['3', '123124234', undefined, 'needle', 'world', 'hay', 2, '3', true, false]))
+
+function findNeedle(haystack) {
+  return `found the needle at position ${haystack.indexOf('needle')}`;
+}
