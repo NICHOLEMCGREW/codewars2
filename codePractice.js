6c17189ebdf50db5c696da1a9a721048dfadfc78
Nichole McGrew <nicholemcgrew@gmail.com>
2024-04-23 07:22:40 -0500

    Calculate BMI

--- codePractice.js
+++ codePractice.js
@@ -392,4 +392,15 @@ const quarterOf = (month) => {
 function smash (words) {
   return String(words).split(',').join(' ')
 };
-console.log(smash(["hello", "world"]))
+// console.log(smash(["hello", "world"]))
+
+
+// Calculate BMI
+function bmi(weight, height) {
+  let bmi = weight/(height**2)
+  if (bmi <= 18.5) return 'Underweight'
+  if (bmi <= 25.0) return 'Normal'
+  if (bmi <= 30.0) return 'Overweight'
+  if (bmi > 30) return 'Obese'
+}
+console.log(bmi(80, 1.80))
