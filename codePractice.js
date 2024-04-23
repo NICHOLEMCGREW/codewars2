fe4d41095930bcd89decbc328c5a4b48b8d2e80d
Nichole McGrew <nicholemcgrew@gmail.com>
2024-04-23 07:40:42 -0500

    Grasshopper - Grade book

--- codePractice.js
+++ codePractice.js
@@ -438,3 +438,13 @@ function greet(name){
     return "Hello, " + name + "!";
   }
 }
+
+// Grasshopper - Grade book
+function getGrade (s1, s2, s3) {
+ let avg = (s1 + s2 + s3) / 3
+ if (avg <= 100 && avg >= 90) return 'A'
+ if (avg < 90 && avg >= 80) return 'B'
+ if (avg < 80 && avg >= 70) return 'C'
+ if (avg < 70 && avg >= 60) return 'D'
+ if (avg < 60) return 'F'
+}
