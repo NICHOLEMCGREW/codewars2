1f70d11e0f353045839637790aea1c797a113732
Nichole McGrew <nicholemcgrew@gmail.com>
2024-05-10 06:25:23 -0500

    created CRUD routes

--- TraversyMedia/expressCrash/server.js
+++ TraversyMedia/expressCrash/server.js
@@ -1,16 +1,20 @@
-import express from 'express'
-import path from 'path';
-import posts from './routes/posts.js';
+import express from "express";
+import path from "path";
+import posts from "./routes/posts.js";
+import exp from "constants";
 
 const port = process.env.PORT || 8000;
 
 const app = express();
 
+//Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 // setup static folder
 // app.use(express.static(path.join(__dirname, 'public')))
 
 //Routes
-app.use('/api/posts', posts)
-
+app.use("/api/posts", posts);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
