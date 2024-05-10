88ee00169f09da5b22063024974a61f96635d88a
Nichole McGrew <nicholemcgrew@gmail.com>
2024-05-10 10:26:23 -0500

    finished express crash

--- TraversyMedia/expressCrash/server.js
+++ TraversyMedia/expressCrash/server.js
@@ -1,9 +1,15 @@
 import express from "express";
 import path from "path";
+import { fileURLToPath } from "url"; //ejs
 import posts from "./routes/posts.js";
-import exp from "constants";
-
+import logger from "./middleware/logger.js";
+import errorHandler from "./middleware/error.js";
+import notFound from "./middleware/notFound.js";
 const port = process.env.PORT || 8000;
+
+// Get directory name
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const app = express();
 
@@ -11,10 +17,18 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+//Logger middleware
+app.use(logger)
+
+
 // setup static folder
-// app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 
 //Routes
 app.use("/api/posts", posts);
+
+//Error handler
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
