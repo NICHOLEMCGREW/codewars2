8f9c486de20205c7070f38f42a782bc8f0c4e135
Nichole McGrew <nicholemcgrew@gmail.com>
2024-05-13 06:24:35 -0500

    Double Char

--- codewars/codePractice.js
+++ codewars/codePractice.js
@@ -888,7 +888,11 @@ function cockroachSpeed(s) {
 
 // Double Char
 function doubleChar(str) {
-  
+  let newStr = ''
+  for (let i = 0; i < str.length; i++) {
+    newStr += str[i] + str[i]
+  }
+  return newStr
 }
 console.log(doubleChar("Adidas"))
 
