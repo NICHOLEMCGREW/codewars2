9dce817c0b6410505234e68b252ee7f240c5c808
Nichole McGrew <nicholemcgrew@gmail.com>
2024-05-14 06:12:54 -0500

    Keep up the hoop

--- codewars/codePractice.js
+++ codewars/codePractice.js
@@ -923,3 +923,12 @@ function buildString(...template){
 function square(num) {
   return num**num
 }
+
+// Keep up the hoop
+function hoopCount (n) {
+  if (n >= 10) {
+    return "Great, now move on to tricks"
+  } else {
+    return "Keep at it until you get it"
+  }
+}
