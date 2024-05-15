490d168ffcf4c1bd32dd48fea0ab3cee8ef515fb
Nichole McGrew <nicholemcgrew@gmail.com>
2024-05-15 05:05:06 -0500

    Multiples!

--- codewars/codePractice.js
+++ codewars/codePractice.js
@@ -933,3 +933,39 @@ function hoopCount (n) {
   }
 }
 
+// Honey to the Bee
+function Bee(capacity, hive) {
+  this.capacity = capacity;
+  this.hive = hive;
+}
+
+function Hive() {
+  this.pollen = 100;
+}
+
+//TODO: The amount of honey in the hive is not increasing.
+Hive.prototype.addPollen = function(pollen) {
+  this.pollen += pollen;
+}
+
+Hive.prototype.getPollen = function() {
+  return this.pollen;
+}
+
+Bee.prototype.unloadPollen = function() {
+  this.hive.addPollen(this.capacity);
+}
+
+
+// Multiples!
+function multiple(x) {
+  if (x % 3 === 0 & x % 5 === 0) {
+    return "BangBoom"
+  } else if (x % 3 === 0) {
+    return "Bang"
+  } else if (x % 5 === 0) {
+    return "Boom"
+  } else {
+    return "Miss"
+  }
+}
