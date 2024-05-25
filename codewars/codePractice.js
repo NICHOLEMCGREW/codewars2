3f261d71976d1f095fc82142d0d3cf3707a3e557
Nichole McGrew <nicholemcgrew@gmail.com>
2024-05-25 07:40:08 -0500

    Switch/Case - Bug Fixing #6

--- codewars/codePractice.js
+++ codewars/codePractice.js
@@ -1066,4 +1066,31 @@ function DNAtoRNA(dna) {
     let newNums = num.map((x) => x * x).join('')
    return +newNums
   }
-  console.log(squareDigits(2112))
+  // console.log(squareDigits(2112))
+
+  // Switch/Case - Bug Fixing #6
+  function evalObject(value){
+    var result = 0;
+    switch(value.operation){
+      case'+': 
+      result = value.a + value.b;
+      break
+      case'-': 
+      result = value.a - value.b;
+      break
+      case'/': 
+      result = value.a / value.b;
+      break
+      case'*': 
+      result = value.a * value.b;
+      break
+      case'%': result = value.a % value.b;
+      break
+      case'^': 
+      result = Math.pow(value.a, value.b);
+      break
+    }
+    return result;
+  }
+
+  console.log(evalObject({a:1,b:1,operation:'+'}))
