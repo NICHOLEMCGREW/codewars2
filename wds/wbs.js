faef61404a469963c65c974e8d059d958aaf972d
Nichole McGrew <nicholemcgrew@gmail.com>
2024-05-28 10:06:28 -0500

    promises

--- wds/wbs.js
+++ wds/wbs.js
@@ -553,3 +553,95 @@ const favoriteAnimal = "cat"
 
 // printName()
 
+// setTimeout(() => {
+//     console.log('inside')
+// }, 1000)
+
+// const button = document.querySelector("button")
+// addClick(button, () => {
+//     console.log('clicked')
+// })
+
+// console.log('outside')
+
+// function addClick(element, callback) {
+//     element.addEventListener('click', callback)
+// }
+
+// const promise = new Promise((resolve, reject) => {
+//     const sum = 1 + 2
+//     if (sum === 2) {
+//         resolve('success')
+//     } else {
+//         reject("Error")
+//     }
+// })
+
+// promise.then(message => {
+//     console.log(message)
+// }).catch(message => {
+//     console.error(message)
+// })
+
+// setTimeout(() => {
+//     console.log("here")
+// }, 250)
+
+// setTimoutPromise(250)
+// 	.then(() => {
+// 		console.log("1")
+// 		return setTimoutPromise(250)
+// 	})
+// 	.then(() => {
+// 		console.log("2")
+// 	})
+// 	.then(() => {
+// 		console.log("3")
+// 	})
+
+// function setTimoutPromise(duration) {
+// 	return new Promise((resolve, reject) => {
+// 		setTimeout(resolve, duration)
+// 	})
+// }
+// const button = document.querySelector('button')
+
+
+// function addEventListenerPromise(element, method) {
+//     return new Promise((resolve, reject) => {
+//         element.addEventListener(method, resolve)
+//     })
+// }
+
+// addEventListenerPromise(button, 'click').then(e => {
+//     console.log('clicked')
+//     console.log(e)
+// })
+
+
+// Promise.allSettled([
+// Promise.reject('error on 1'),
+// Promise.resolve('2'),
+// Promise.resolve('3'),
+// ]).then(messages => {
+//     console.log(messages)
+// }).catch(error => {
+//     console.log(error)
+// });
+
+// Promise.resolve('value').then(message => console.log('resoled'))
+
+
+
+const promise = Promise.resolve('here')
+
+promise
+.then(message => {
+    console.log(message)
+})
+.catch(error => {
+    console.log(error)
+})
+.finally(() => {
+    console.log('finally')
+})
