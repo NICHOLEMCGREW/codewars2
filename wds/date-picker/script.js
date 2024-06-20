05c17acb5f6acbed777921160d2912b8b7fa0a90
Nichole McGrew <nicholemcgrew@gmail.com>
2024-06-20 11:10:04 -0500

    commit changes

---
+++ wds/date-picker/script.js
@@ -0,0 +1,7 @@
+// import printUser, { me as kyle, sally } from './user.js'
+
+// printUser(kyle)
+// printUser(sally)
+
+// import user from './user.js';
+
---
+++ wds/date-picker/user.js
@@ -0,0 +1,19 @@
+export const me = {
+    name: "Kyle",
+    age: 25
+}
+
+export const sally = {
+    name: "Sally",
+    age: 22
+}
+
+function printUser(user) {
+    console.log(`Name: ${user.name}. Age: ${user.age}`)
+}
+
+module.exports = {
+    me: me,
+    sally: sally,
+    printUser: printUser
+}
