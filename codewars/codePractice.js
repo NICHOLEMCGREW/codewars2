adeb07ce6978d54ecf46f105ff71215c0e56d841
Nichole McGrew <nicholemcgrew@gmail.com>
2024-07-06 08:56:27 -0500

    Neutralisation

--- codewars/codePractice.js
+++ codewars/codePractice.js
@@ -1149,3 +1149,17 @@ function sameCase(a, b){
     return 0
   }
 }
+
+// Neutralisation
+function neutralise(s1, s2) {
+  let result = ''
+    for (let i = 0; i < s1.length; i++) {
+if (s1[i] === s2[i]) {
+  result += s1[i]
+} else {
+  result += '0'
+}
+    }
+    return result
+}
+console.log(neutralise("--++--", "++--++"))
---
+++ wds/js-need-to-know/tut.js
@@ -0,0 +1,56 @@
+// ;<button
+// 	onclick={() => {
+// 		console.log("ello!")
+// 	}}
+// ></button>
+
+// const MyComponent = () => {
+// 	return <div></div>
+// }
+
+// export const DoSomething = () => {}
+
+// // let age = 16
+// // let name = age > 10 ? "Nichole" : "Pedro"
+
+// const Component = () => {
+// 	return age > 10 ? <div>Pedro</div> : <div>Jack</div>
+// }
+
+// // const name = 'Pedro'
+
+// const person = {
+// 	name: "Pedro",
+// 	age: 20,
+// 	isMarried: false,
+// }
+
+// const { name, age, isMarried } = person
+
+// const person2 = {...person, name: "Jack"}
+
+// const names = ['pedro', 'jack', 'jessica']
+// const names2 = [...names, 'Joel']
+
+// .map()
+// .filter()
+// .reduce()
+
+let names = ['pedro', 'jessica', 'carol']
+
+names.map((name) => {
+return name + '1'
+
+})
+
+
+names.map((name) => {
+    return <h1>{name}</h1>
+})
+
+names.filter((name) => {
+    return name !== 'pedro'
+})
+
+//Async + Await + Fetch-- Promises and Async Await -- Fetch Data 
+
